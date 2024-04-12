850e757943b27591638c344b91beee6cb000a88d
Egor Ilchenko <egor.ilchenko.u@gmail.com>
2024-04-11 22:34:31 -0400

    project set-up

---
+++ Lesson 8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 8
+//
+//  Created by Egor Ilchenko on 4/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
